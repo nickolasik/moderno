4b448fc585451de64a016183555c3aacc3479e1d
nickolasik <mr.nick-sol@yandex.ru>
2021-01-25 12:01:00 +0300

    added product-slider & product

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,14 @@
 $(function(){
+    $(".rate-star").rateYo({
+        starWidth: "12px",
+        rating: 5,
+        readOnly: true
+    });
+    $('.product-slider__inner').slick({
+        dots:true,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+        arrows: false
+    });
     var mixer = mixitup('.products__inner-box');
 });
