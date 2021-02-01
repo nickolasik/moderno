34d19472348fba4f3fcadad8e9c84eb5b995e806
nickolasik <mr.nick-sol@yandex.ru>
2021-02-01 13:21:29 +0300

    adaptive header and footer

--- app/js/main.js
+++ app/js/main.js
@@ -10,5 +10,17 @@ $(function(){
         slidesToScroll: 4,
         arrows: false
     });
+
+    $('.header__btn-menu').on('click', function(){
+        $('.header__box').toggleClass('active');
+    });
+
+    $('.menu__btn').on('click', function(){
+        $('.menu__list').slideToggle();
+    });
+
+
+
+
     var mixer = mixitup('.products__inner-box');
 });
