36ab58901044d790be5a6f3d1427b71485fb010c
nickolasik <mr.nick-sol@yandex.ru>
2021-02-05 14:42:30 +0300

    added 404, about, contact

--- app/js/main.js
+++ app/js/main.js
@@ -10,13 +10,19 @@ $(function(){
         slidesToScroll: 4,
         arrows: false
     });
-
     $('.header__btn-menu').on('click', function(){
         $('.header__box').toggleClass('active');
     });
-
     $('.menu__btn').on('click', function(){
         $('.menu__list').slideToggle();
+    });
+    $('.product-one__tabs .tab').on('click', function (event) {
+        var id = $(this).attr('data-id');
+        $('.product-one__tabs').find('.tab-item').removeClass('active-tab').hide();
+        $('.product-one__tabs .tabs').find('.tab').removeClass('active');
+        $(this).addClass('active');
+        $('#' + id).addClass('active-tab').fadeIn();
+        return false;
     });
 
 
